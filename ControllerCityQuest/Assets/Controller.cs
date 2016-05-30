e860dba79f2ff66a567a1a3e468ef662b0c38b1b
AlinaEcke <shumovaa@tcd.ie>
2016-05-31 00:10:14 +0100

    Added Controller script to move Sphere with left analogue stick
    Added Controller script to move Sphere with left analogue stick

---
+++ ControllerCityQuest/Assets/Controller.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		Debug.Log("H:" + Input.GetAxis("Horizontal"));
+		Debug.Log("V:" + Input.GetAxis("Vertical"));
+		Rigidbody rigidBodyVar = gameObject.GetComponent<Rigidbody>();
+		rigidBodyVar.velocity = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+	}
+}
