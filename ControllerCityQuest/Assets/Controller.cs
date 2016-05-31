e324a0ab1405eb6f41bea3ce031a149ca191a99a
AlinaEcke <shumovaa@tcd.ie>
2016-05-31 18:24:21 +0100

    Camera is a child object of the avatar, some code added

--- ControllerCityQuest/Assets/Controller.cs
+++ ControllerCityQuest/Assets/Controller.cs
@@ -15,5 +15,16 @@ public class Controller : MonoBehaviour {
 		Debug.Log("V:" + Input.GetAxis("Vertical"));
 		Rigidbody rigidBodyVar = gameObject.GetComponent<Rigidbody>();
 		rigidBodyVar.velocity = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        Transform cameraTransform = transform.Find("MainCamera");
+        Vector3 forwardVector3 = cameraTransform.forward;
+        Vector3 forwardVector2 = new Vector3(forwardVector3.x, 0f, forwardVector3.z);
+        Vector3 sidewaysVector3 = cameraTransform.right;
+        Vector3 sidewaysVector2 = new Vector3(sidewaysVector3.x, 0f, sidewaysVector3.z);
+
+        forwardVector2 = forwardVector2 * Input.GetAxis("Vertical");
+        sidewaysVector2 = sidewaysVector2 * Input.GetAxis("Horizontal");
+
+        rigidBodyVar.velocity = forwardVector2 + sidewaysVector2;
 	}
 }
